74bd16e06d502c13ae07628449839f99e626c6b1
Tran Hoang Thien <Martin.Tran@legato.co>
2022-08-02 08:58:21 +0700

    add footer and SEO page Home

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -1,10 +1,45 @@
-import { Box } from "@mui/material";
+import { Facebook, Instagram, LinkedIn, Twitter } from "@mui/icons-material";
+import { Box, Icon, Stack, Typography } from "@mui/material";
 import React from "react";
 
 export function Footer() {
+  const socialLinks = [
+    {
+      icon: Facebook,
+      url: "https://www.facebook.com/",
+    },
+    {
+      icon: Instagram,
+      url: "https://www.facebook.com/",
+    },
+    {
+      icon: Twitter,
+      url: "https://www.facebook.com/",
+    },
+    {
+      icon: LinkedIn,
+      url: "https://www.facebook.com/",
+    },
+  ];
   return (
     <Box component="footer" py={2} textAlign="center">
-      Footer
+      <Stack direction="row" justifyContent="center">
+        {socialLinks.map((item, idx) => (
+          <Box
+            component="a"
+            key={idx}
+            p={2}
+            href={item.url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Icon component={item.icon} sx={{ fontSize: 48 }} />
+          </Box>
+        ))}
+      </Stack>
+      <Typography>
+        Copyright ©{new Date().getFullYear()} All rights reserved
+      </Typography>
     </Box>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Seo from "@/components/common/seo";
 import { FeatureWorks, HeroSection, RecentPosts } from "@/components/home";
 import MainLayout from "@/components/layout/main";
 import { Box } from "@mui/material";
@@ -18,6 +19,16 @@ const Home: NextPageWithLayout = () => {
 
   return (
     <Box>
+      <Seo
+        data={{
+          title: "Blog cá nhân",
+          description:
+            "With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!",
+          url: "https://learn-nextjs-2022.vercel.app/",
+          thumbnailUrl:
+            "https://res.cloudinary.com/dmj3asaf3/image/upload/v1648962369/cld-sample.jpg",
+        }}
+      />
       <HeroSection />
       <RecentPosts />
       <FeatureWorks />
